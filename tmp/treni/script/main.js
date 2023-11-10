637bda0f698e1b7a23cfbdb12340d402336d803b
nonnt66v2 <91503962+nonnt66v2@users.noreply.github.com>
2023-11-10 16:48:22 +0100

    fetch try

--- tmp/treni/script/main.js
+++ tmp/treni/script/main.js
@@ -1,7 +1,13 @@
 var p = document.getElementById("testo");
-file_get_contents("https://www.lefrecce.it/Channels.Website.BFF.WEB/website/locations/search?name=napoli")
-function file_get_contents(filename) {
-    fetch(filename,{mode : "no-cors"}).then((resp) => resp.text()).then(function(data) {
-        p.innerHTML = data;
+const URL = new Request("https://lefrecce.it/Channels.Website.BFF.WEB/website/locations/search?name=%22napoli%22&limit=100000");
+fetch(URL, {
+        method: "GET",
+        mode: "no-cors"
+    }
+).then(async response => {
+    p.innerHTML = await response.json().then(data => {
+        data.text();
     });
-}
+}).catch((err) => {
+        p.innerHTML = "<h1>errore nel caricamento delle tuple</h1>";
+    });

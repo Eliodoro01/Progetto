9e8e3576a4546b2d756538c14bae7d87c075b3c1
nonnt66v2 <91503962+nonnt66v2@users.noreply.github.com>
2023-11-10 12:09:46 +0100

    Update main.js

--- tmp/treni/script/main.js
+++ tmp/treni/script/main.js
@@ -1,22 +1,7 @@
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(
-        policy =>
-        {
-            policy.WithOrigins("http://localhost:19008")
-                .AllowAnyHeader()
-                .AllowAnyMethod();
-        });
-});
-var app = builder.Build();
-app.UseCors();
 var p = document.getElementById("testo");
-fetch('https://www.lefrecce.it/Channels.Website.BFF.WEB/website/locations/search?name=napoli', { mode: 'no-cors'})
-    .then(data => {
-        console.table(data);
-        return data;
-    })
-    .catch(e => {
-        console.log(e);
-        return e;
+file_get_contents("https://www.lefrecce.it/Channels.Website.BFF.WEB/website/locations/search?name=napoli")
+function file_get_contents(filename) {
+    fetch(filename,{mode : "no-cors"}).then((resp) => resp.text()).then(function(data) {
+        p.innerHTML = data;
     });
+}
